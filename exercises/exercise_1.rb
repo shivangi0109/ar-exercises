0500e2b987cb25d3997d017ccd62dec12ba10d5d
Shivangi Srivastava <shi06dec@gmail.com>
2023-10-18 15:08:02 -0600

    Created 3 stores in the database and ensure by using count that there are three stores in the database

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,11 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+# 1. Create 3 stores
+Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+# 2. Output the number of stores
+puts "Number of stores: #{Store.count}"
