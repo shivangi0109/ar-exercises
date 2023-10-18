58e4787bdbfc223cb48591457e35572a734177a2
Shivangi Srivastava <shi06dec@gmail.com>
2023-10-18 15:51:32 -0600

    Exercise 5: Calculations using sum, average, where and count

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# Output the total revenue for all stores
+total_revenue = Store.sum(:annual_revenue)
+puts "Total Revenue for the Company: $#{total_revenue}"
+
+# Output the average annual revenue for all stores
+average_revenue = Store.average(:annual_revenue)
+puts "Average Annual Revenue for All Stores: $#{average_revenue}"
+
+# Output the number of stores generating $1M or more in annual sales
+high_revenue_stores_count = Store.where('annual_revenue >= ?', 1000000).count
+puts "Number of Stores with Annual Sales $1M or More: #{high_revenue_stores_count}"
