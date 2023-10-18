94901615a72dbe3f11c93477f13a5efa77bfb18e
Shivangi Srivastava <shi06dec@gmail.com>
2023-10-18 15:22:15 -0600

    Exercise 2 - Updated the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# 1. Load the first store
+@store1 = Store.find(1)
+
+# 2. Load the second store
+@store2 = Store.find(2)
+
+# 3. Update the first store
+@store1.update(name: 'Edmonton')
+
+# Output the updated store information
+puts "Updated store name: #{@store1.name}"
