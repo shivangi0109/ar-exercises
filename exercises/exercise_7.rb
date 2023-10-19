2e4947d0d571f3d3dc858283457da30887a05c5b
Shivangi Srivastava <shi06dec@gmail.com>
2023-10-19 13:32:20 -0600

    Exercise 7: Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,20 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# Ask the user for a store name
+print "Enter a store name: "
+store_name = gets.chomp
+
+# Attempt to create a store with the inputted name (leave out other fields)
+new_store = Store.new(name: store_name) # This will show error messages
+
+# new_store = Store.new(name: store_name, annual_revenue: 6000000, mens_apparel: true) # This won't show error messages because it is validating everything and saving
+
+# Display error messages if save/create fails
+unless new_store.save
+  puts "Errors:"
+  new_store.errors.full_messages.each do |error|
+    puts error
+  end
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,16 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_carry_apparel
+
+  def must_carry_apparel
+    # This condition `unless mens_apparel || womens_apparel` checks if the store has either mens_apparel set to true or womens_apparel set to true.
+    # If the condition is not met (i.e., the store doesn't carry either men's or women's apparel), it adds an error message to the :base context using errors.add(:base, "Stores must carry at least one type of apparel").
+    # In simpler terms, this validation is ensuring that a store must have at least one type of apparel. If a store is created or updated without specifying either `mens_apparel`` or `womens_apparel`, it will trigger this validation error. 
+    unless mens_apparel || womens_apparel
+      # Using :base is helpful when you have validations that involve multiple attributes or the overall state of the object rather than just one attribute.
+      errors.add(:base, "Stores must carry at least one type of apparel")
+    end
+  end
 end
